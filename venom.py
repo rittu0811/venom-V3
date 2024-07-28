5b634f73d3fffce69e6885002f45706374a6bc6f
rittu0811 <rittu64742raj@gmail.com>
2024-07-28 21:55:30 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7484064527:AAH10vOK504NW_Kh37YB7thRqwLk1-8hs5w'
+ADMIN_ID = '7176249861'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
